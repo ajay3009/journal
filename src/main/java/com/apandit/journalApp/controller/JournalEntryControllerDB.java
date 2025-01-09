9c355a567e79c749950955d4758cbf351a834221
Ajay.Pandit <ajaysandy@gmail.com>
2025-01-09 18:43:03 +0530

    Journal Entry apis update based on username

--- src/main/java/com/apandit/journalApp/controller/JournalEntryControllerDB.java
+++ src/main/java/com/apandit/journalApp/controller/JournalEntryControllerDB.java
@@ -1,7 +1,9 @@
 package com.apandit.journalApp.controller;
 
 import com.apandit.journalApp.entity.JournalEntry;
+import com.apandit.journalApp.entity.User;
 import com.apandit.journalApp.service.JournalEntryService;
+import com.apandit.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,20 +20,23 @@ public class JournalEntryControllerDB {
     @Autowired
     private JournalEntryService journalEntryService;
 
-    @GetMapping
-    public ResponseEntity<List<JournalEntry>> getAll() {
-        List<JournalEntry> all = journalEntryService.getAll();
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("{userName}")
+    public ResponseEntity<List<JournalEntry>> getAllEntriesByUser(@PathVariable String userName) {
+        User user = userService.findByUserName(userName);
+        List<JournalEntry> all = user.getJournalEntry();
         if (all != null && !all.isEmpty()) {
             return new ResponseEntity<>(all, HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping
-    public ResponseEntity<JournalEntry> creatEntry(@RequestBody JournalEntry myEntry) {
+    @PostMapping("{userName}")
+    public ResponseEntity<JournalEntry> creatEntry(@RequestBody JournalEntry myEntry, @PathVariable String userName) {
         try {
-            myEntry.setDate(LocalDateTime.now());
-            journalEntryService.saveEntry(myEntry);
+            journalEntryService.saveEntry(myEntry, userName);
             return new ResponseEntity<>(myEntry, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -48,15 +53,15 @@ public class JournalEntryControllerDB {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("/id/{myId}")
-    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId) {
+    @DeleteMapping("/id/{userName}/{myId}")
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable String userName, @PathVariable ObjectId myId) {
 
-        journalEntryService.deleteById(myId);
+        journalEntryService.deleteById(userName, myId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping("/id/{myId}")
-    public ResponseEntity<?> updateJournalEntryById(@PathVariable ObjectId myId, @RequestBody JournalEntry myEntry) {
+    @PutMapping("/id/{userName}/{myId}")
+    public ResponseEntity<?> updateJournalEntryById(@PathVariable ObjectId myId, @PathVariable String userName, @RequestBody JournalEntry myEntry) {
         JournalEntry oldEntry = journalEntryService.getEntryById(myId).orElse(null);
         if (oldEntry != null) {
             oldEntry.setTitle(myEntry.getTitle() != null && !myEntry.getTitle().equals("") ? myEntry.getTitle() : oldEntry.getTitle());
--- src/main/java/com/apandit/journalApp/service/JournalEntryService.java
+++ src/main/java/com/apandit/journalApp/service/JournalEntryService.java
@@ -1,11 +1,13 @@
 package com.apandit.journalApp.service;
 
 import com.apandit.journalApp.entity.JournalEntry;
+import com.apandit.journalApp.entity.User;
 import com.apandit.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,8 +17,15 @@ public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
 
-    public void saveEntry(JournalEntry journalEntry) {
-        journalEntryRepository.save(journalEntry);
+    @Autowired
+    private UserService userService;
+
+    public void saveEntry(JournalEntry journalEntry, String userName) {
+        User user = userService.findByUserName(userName);
+        journalEntry.setDate(LocalDateTime.now());
+        JournalEntry item = journalEntryRepository.save(journalEntry);
+        user.getJournalEntry().add(item);
+        userService.saveEntry(user);
     }
 
     public List<JournalEntry> getAll() {
@@ -27,7 +36,14 @@ public class JournalEntryService {
         return journalEntryRepository.findById(id);
     }
 
-    public void deleteById(ObjectId id) {
+    public void deleteById(String userName, ObjectId id) {
+        User user = userService.findByUserName(userName);
+        user.getJournalEntry().removeIf(x -> x.getId().equals(id));
+        userService.saveEntry(user);
         journalEntryRepository.deleteById(id);
+    }
+
+    public void saveEntry(JournalEntry oldEntry) {
+        journalEntryRepository.save(oldEntry);
     }
 }
