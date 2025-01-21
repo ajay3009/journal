46239efbe13d1d7414058598e760763934ed9576
Ajay.Pandit <ajaysandy@gmail.com>
2025-01-21 15:57:39 +0530

    AutoWired updated

--- src/main/java/com/apandit/journalApp/config/SpringSecurity.java
+++ src/main/java/com/apandit/journalApp/config/SpringSecurity.java
@@ -4,11 +4,8 @@ import com.apandit.journalApp.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,7 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SpringSecurity {
 
-
+    @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
     @Bean
