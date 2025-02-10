ca0083e23e0c2594c949e082dd2c28233d3752d3
Ajay.Pandit <ajaysandy@gmail.com>
2025-02-10 10:54:05 +0530

    JUnit User  serivce impl test with Mock Bean

--- src/test/java/com/apandit/journalApp/service/UserDetailsServiceImplTests.java
+++ src/test/java/com/apandit/journalApp/service/UserDetailsServiceImplTests.java
@@ -1,22 +1,35 @@
 package com.apandit.journalApp.service;
 
+import com.apandit.journalApp.entity.User;
 import com.apandit.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
 
 import static org.mockito.Mockito.when;
 
 public class UserDetailsServiceImplTests {
 
-    @Autowired
+    @InjectMocks
     private UserDetailsServiceImpl userDetailsService;
 
-    @Mock
+    @MockBean
     private UserRepository userRepository;
 
+    @Test
     void loadUserByUsernameTest() {
-        when(userRepository.findByUserName()).thenReturn()
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("Ram").password("vvvvvvsvsvv").roles(new ArrayList<>()).build());
         UserDetails user = userDetailsService.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
     }
 }
