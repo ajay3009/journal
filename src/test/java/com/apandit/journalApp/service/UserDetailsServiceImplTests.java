a429b707736c7c65e61744c9c27af298cec90ccc
Ajay.Pandit <ajaysandy@gmail.com>
2025-02-11 10:03:25 +0530

    JUnit User  serivce impl test with Mock

--- src/test/java/com/apandit/journalApp/service/UserDetailsServiceImplTests.java
+++ src/test/java/com/apandit/journalApp/service/UserDetailsServiceImplTests.java
@@ -3,10 +3,12 @@ package com.apandit.journalApp.service;
 import com.apandit.journalApp.entity.User;
 import com.apandit.journalApp.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -23,8 +25,13 @@ public class UserDetailsServiceImplTests {
     @InjectMocks
     private UserDetailsServiceImpl userDetailsService;
 
-    @MockBean
+    @Mock
     private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     void loadUserByUsernameTest() {
