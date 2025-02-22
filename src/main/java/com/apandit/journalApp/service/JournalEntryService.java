d2e8e3698146b4238f22bae9b36ec95270ce63b3
Ajay.Pandit <ajaysandy@gmail.com>
2025-02-22 10:15:07 +0530

    sonar qube code smells fixed

--- src/main/java/com/apandit/journalApp/service/JournalEntryService.java
+++ src/main/java/com/apandit/journalApp/service/JournalEntryService.java
@@ -3,6 +3,7 @@ package com.apandit.journalApp.service;
 import com.apandit.journalApp.entity.JournalEntry;
 import com.apandit.journalApp.entity.User;
 import com.apandit.journalApp.repository.JournalEntryRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class JournalEntryService {
 
     @Autowired
@@ -54,7 +56,7 @@ public class JournalEntryService {
                 journalEntryRepository.deleteById(id);
             }
         } catch (Exception e) {
-            System.out.println(e);
+            log.error("Error", e);
             throw new RuntimeException("An error occured while deleting the entry", e);
         }
         return removed;
