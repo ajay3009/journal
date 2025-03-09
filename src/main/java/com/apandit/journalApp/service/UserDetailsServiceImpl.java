9a5b2532d4950454fe42cd65ec7b01d6decaf3d4
Ajay.Pandit <ajaysandy@gmail.com>
2025-03-09 09:52:13 +0530

    Unused import removed and service annotation used

--- src/main/java/com/apandit/journalApp/service/UserDetailsServiceImpl.java
+++ src/main/java/com/apandit/journalApp/service/UserDetailsServiceImpl.java
@@ -3,15 +3,13 @@ package com.apandit.journalApp.service;
 import com.apandit.journalApp.entity.User;
 import com.apandit.journalApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
