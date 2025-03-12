9ae99d6b7ed418136d884b11124689a333448383
Ajay.Pandit <ajaysandy@gmail.com>
2025-03-12 09:16:24 +0530

    Unused import removed and service annotation used

--- src/main/java/com/apandit/journalApp/repository/UserRepository.java
+++ src/main/java/com/apandit/journalApp/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.apandit.journalApp.repository;
 
-import com.apandit.journalApp.entity.JournalEntry;
 import com.apandit.journalApp.entity.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
