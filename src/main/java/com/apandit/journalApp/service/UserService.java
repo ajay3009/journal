cabb917822581d2e4887ce6600eb3ad484b1f470
Ajay.Pandit <ajaysandy@gmail.com>
2025-03-14 15:07:29 +0530

    Unused import removed and service annotation used

--- src/main/java/com/apandit/journalApp/service/UserService.java
+++ src/main/java/com/apandit/journalApp/service/UserService.java
@@ -7,7 +7,6 @@ import com.apandit.journalApp.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
