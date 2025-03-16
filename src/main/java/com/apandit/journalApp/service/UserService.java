4320531e12aec4acc2b98c4aac0eb2ba4066e5c6
Ajay.Pandit <ajaysandy@gmail.com>
2025-03-16 23:41:57 +0530

    Unused import removed and service annotation used

--- src/main/java/com/apandit/journalApp/service/UserService.java
+++ src/main/java/com/apandit/journalApp/service/UserService.java
@@ -2,7 +2,6 @@ package com.apandit.journalApp.service;
 
 import com.apandit.journalApp.entity.JournalEntry;
 import com.apandit.journalApp.entity.User;
-import com.apandit.journalApp.repository.JournalEntryRepository;
 import com.apandit.journalApp.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
