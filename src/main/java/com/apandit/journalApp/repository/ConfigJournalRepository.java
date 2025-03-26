99340a035ba26c53d4619b26d2bde3da3d82bcf5
Ajay.Pandit <ajaysandy@gmail.com>
2025-03-26 20:12:51 +0530

    sonar fixes

--- src/main/java/com/apandit/journalApp/repository/ConfigJournalRepository.java
+++ src/main/java/com/apandit/journalApp/repository/ConfigJournalRepository.java
@@ -1,7 +1,6 @@
 package com.apandit.journalApp.repository;
 
 import com.apandit.journalApp.entity.ConfigJournalAppEntity;
-import com.apandit.journalApp.entity.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
