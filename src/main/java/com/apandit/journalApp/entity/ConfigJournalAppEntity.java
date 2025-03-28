31fcef55b19dd40efe88e0d1bd19ca372941d44b
Ajay.Pandit <ajaysandy@gmail.com>
2025-03-28 23:34:46 +0530

    sonar fixes

--- src/main/java/com/apandit/journalApp/entity/ConfigJournalAppEntity.java
+++ src/main/java/com/apandit/journalApp/entity/ConfigJournalAppEntity.java
@@ -4,9 +4,6 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Document(collection = "config_journal_app")
 @Data
 @Builder
